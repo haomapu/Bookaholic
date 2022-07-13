575c05e8c3332ad684914f76888c05b6bc674385
haomapu <haoanh7702@gmail.com>
2022-07-13 22:12:06 +0700

    Connect BE

--- DescripPage/src/BookGeneral/book.js
+++ DescripPage/src/BookGeneral/book.js
@@ -2,13 +2,25 @@ import React from 'react'
 import godfather from './godfather.jpg'
 import './book.css'
 import Sidebar from "../sidebar/sidebar";
+import { useState } from 'react'
+import axios from "axios";
 
 export default function Book() {
+  const [text, setText] = useState("");
+  
+  function getQuote() {
+      axios.get("http://26.0.202.224:3000/book",  { crossdomain: true }).then(response => {
+        setText(response.data)
+      }).catch((error)=>{
+        console.log("Error",error);
+      });
+  }
+  getQuote()
   return (
     <div class="BookDes">
         <img class="BookPic" src={godfather} alt="Godfather" ></img>
         <div class="Des">
-          <span class="Title">The Godfather</span>
+          <span class="Title">{text[0].title}</span>
           <span class="Author">Author: Mario Puzo</span>
           <span class="Description">The Godfather by Mario Puzo (Author), Illustrated by Robert Carter, Introduced by Jonathan Freedland. Mario Puzo’s brilliant and brutal story of Mafia feuds and retribution in post-war New York is published in a sensational new illustrated Folio Society edition, introduced...</span>   
           <div class="rateStar">
