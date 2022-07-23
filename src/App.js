6c6a1fcd70b127eea23fc789233d4bbf2fd23ddb
Hoang Mai <maihoang2152002@gmail.com>
2022-07-24 01:22:09 +0700

    link pages and fix some issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import TopBar from "./components/topbar/TopBar";
 import Homepage from "./pages/homepage/Homepage";
+import LoginPage from "./LoginPage/LoginPage";
+import DescripPage from "./DescripPage/DescripPage";
 import Settings from "./pages/settingspage/Settings";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+    const currentUser = false;
     return (
         <Router>
             <TopBar />
             <Routes>
                 <Route exact path="/" element={<Homepage />} />
-                <Route path="/settings" element={<Settings />} />
+                <Route
+                    path="/login"
+                    element={currentUser ? <Homepage /> : <LoginPage />}
+                />
+                <Route
+                    path="/settings"
+                    element={currentUser ? <Settings /> : <LoginPage />}
+                />
+                <Route path="/book/abc" element={<DescripPage />} />
             </Routes>
         </Router>
     );
--- src/DescripPage/BookGeneral/book.js
+++ src/DescripPage/BookGeneral/book.js
@@ -1,63 +1,67 @@
-import React from 'react'
-import './book.css'
-import Sidebar from "../sidebar/sidebar";
+import React from "react";
+import "./book.css";
+// import Sidebar from "../sidebar/sidebar";
+import Sidebar from "../../components/sidebar/Sidebar";
+
 import { useEffect, useState } from "react";
 import axios from "axios";
 var flag = 0;
-let AuthorName = ""
+let AuthorName = "";
 
 export default function Book() {
-  const [Title, setTitle] = useState("");
-  const [Author, setAuthor] = useState("");
-  
-  useEffect(() => {
-      const fetchTitle = async () => {
-          const res = await axios.get("http://localhost:5000/book");
-          setTitle(res.data[0]);
-      };
-      fetchTitle();
-  }, []);
-  useEffect(() => {
-    const fetchAuthor = async () => {
-        const res = await axios.get("http://localhost:5000/author");
-        setAuthor(res.data);
-    };
-    fetchAuthor();
-  }, []);
+    const [Title, setTitle] = useState("");
+    const [Author, setAuthor] = useState("");
 
-  function getName() {
-    Object.keys(Author).map(a => {
-      if (Title.author === Author[a]._id && flag === 0)
-      {
-        AuthorName = Author[a].name
-        flag = 1;
-      }
-        
-    })
-  }
-  
-  getName()
+    useEffect(() => {
+        const fetchTitle = async () => {
+            const res = await axios.get("http://localhost:5000/book");
+            setTitle(res.data[0]);
+        };
+        fetchTitle();
+    }, []);
+    useEffect(() => {
+        const fetchAuthor = async () => {
+            const res = await axios.get("http://localhost:5000/author");
+            setAuthor(res.data);
+        };
+        fetchAuthor();
+    }, []);
 
-  return (
-    <div className="BookDes">
-        <img className="BookPic" src= "https://images-na.ssl-images-amazon.com/images/I/61cfS2XXyEL.jpg" alt="Godfather" ></img>
-        <div className="Des">
-          <span className="Title">{Title.title}</span>
-          <span className="Author">Author: {AuthorName}</span>
-          <span className="Description">{Title.description}</span>   
-          <div className="rateStar">
-            <span className="fa fa-star checked"></span>
-            <span className="fa fa-star checked"></span>
-            <span className="fa fa-star checked"></span>
-            <span className="fa fa-star"></span>
-            <span className="fa fa-star"></span>
-          </div>
-          <div className="Fav">    
-            <button className="FavBut">Favorite</button>
-            <button className="FavBut">Wishlist</button>
-          </div>   
+    function getName() {
+        Object.keys(Author).map(a => {
+            if (Title.author === Author[a]._id && flag === 0) {
+                AuthorName = Author[a].name;
+                flag = 1;
+            }
+        });
+    }
+
+    getName();
+
+    return (
+        <div className="BookDes">
+            <img
+                className="BookPic"
+                src="https://images-na.ssl-images-amazon.com/images/I/61cfS2XXyEL.jpg"
+                alt="Godfather"
+            ></img>
+            <div className="Des">
+                <span className="Title">{Title.title}</span>
+                <span className="Author">Author: {AuthorName}</span>
+                <span className="Description">{Title.description}</span>
+                <div className="rateStar">
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                </div>
+                <div className="Fav">
+                    <button className="FavBut">Favorite</button>
+                    <button className="FavBut">Wishlist</button>
+                </div>
+            </div>
+            <Sidebar />
         </div>
-        <Sidebar /> 
-    </div>
-  )
+    );
 }
