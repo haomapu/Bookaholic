3dd2fa32f8e3fb971c2864f7b3dccf20a1329776
Hoang Mai <maihoang2152002@gmail.com>
2022-07-24 00:52:43 +0700

    index and app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import TopBar from "./components/topbar/TopBar";
+import Homepage from "./pages/homepage/Homepage";
+import Settings from "./pages/settingspage/Settings";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import DescripPage from './DescripPage/DescripPage';
-import LoginPage from './LoginPage/LoginPage';
 function App() {
-  return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<DescripPage />} />
-        <Route path="/login" element={<LoginPage />} />
-    </Routes>
-    </BrowserRouter>
-
-
-  );
+    return (
+        <Router>
+            <TopBar />
+            <Routes>
+                <Route exact path="/" element={<Homepage />} />
+                <Route path="/settings" element={<Settings />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
