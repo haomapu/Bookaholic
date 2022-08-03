78c86da8445e3fee331ebef9c4ec151aac54b487
baonguyen25 <nguyenminhgiabao2501@gmail.com>
2022-08-03 22:50:47 +0700

    fix some models and logic

---
+++ Backend/express-mongodb-api-main/controllers/userController.js
@@ -0,0 +1,53 @@
+const User = require("../model/account");
+
+const userController = {
+
+    getAllUser : async (req, res) => {
+        try{
+            const users = await User.find();
+            res.status(200).json(users);
+        }catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    
+    createUser : async (req, res) => {
+        try{
+            const newUser = new User(req.body);
+            const savedUser = await newUser.save();
+            res.status(200).json(savedUser);
+        }catch(err) {
+            res.status(500).json(err);
+        }
+    },
+
+    getUser : async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            res.status(200).json(user);
+        }catch(err) {
+            res.status(500).json(err);
+        }
+    },
+
+    updateUser : async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            await user.updateOne({$set : req.body});
+            res.status(200).json("Update Successfully");
+        }catch(err) {
+            res.status(500).json(err);
+        }
+    },
+
+    deleteUser : async (req, res) => {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Delete Successfully");
+        }catch(err) {
+            res.status(500).json(err);
+        }
+    }
+};
+
+module.exports = userController
---
+++ Backend/express-mongodb-api-main/model/account.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Book = require("./book");
+
+const accountSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        maxLength: 30,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        maxLength: 30,
+        required: true
+    },
+
+    username: {
+        type: String,
+        maxLength: 30,
+        required: true
+    },
+
+    password: {
+        type: String,
+        maxLength: 30,
+        minLength: 6,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+    },
+
+    admin: {
+        type: String,
+        default: "0"
+    },
+
+    urlAvatar: {
+        type: String,
+    },
+
+    wishlist: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: Book,
+        },
+    ]
+});
+
+let Account = mongoose.model("Account", accountSchema);
+module.exports = Account;
