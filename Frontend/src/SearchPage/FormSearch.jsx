23be6b41a7e8c03ed129044648d8c90a3f0b6f6d
Hoang Mai <maihoang2152002@gmail.com>
2022-08-13 00:14:50 +0700

    search layout

--- Frontend/src/SearchPage/FormSearch.jsx
+++ Frontend/src/SearchPage/FormSearch.jsx
@@ -9,76 +9,49 @@ export default function FormSearch() {
                         <h2> Title</h2>
                         <div className="b">
                             <div className="input">
-                                    <input type="text" name="" />
+                                <input type="text" name="" />
                             </div>
                             <div className="searchBut">
-                                <button type="Search">
-                                    Search
-                                </button>
+                                <button type="Search">Search</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="tickSearch">
-                        <h3>
-                            Lifestyle:
-                            <input
-                                type="checkbox"
-                                id="option-a"
-                                value="Lifestyle"
-                            />
-                        </h3>
-                        <h3>
-                            Self help:
-                            <input
-                                type="checkbox"
-                                id="option-b"
-                                value="Selfhelp"
-                            />
-                        </h3>
-                        <h3>
-                            Novel:
-                            <input
-                                type="checkbox"
-                                id="option-c"
-                                value="Novel"
-                            />
-                        </h3>
-                        <h3>
-                            Sport:
-                            <input
-                                type="checkbox"
-                                id="option-d"
-                                value="Sport"
-                            />
-                        </h3>
-                        <h3>
-                            Sport:
-                            <input
-                                type="checkbox"
-                                id="option-e"
-                                value="Sport"
-                            />
-                        </h3>
-                        <h3>
-                            Sport:
-                            <input
-                                type="checkbox"
-                                id="option-f"
-                                value="Sport"
-                            />
-                        </h3>
-                        <h3>
-                            Sport:
-                            <input
-                                type="checkbox"
-                                id="option-g"
-                                value="Sport"
-                            />
-                        </h3>
-                    </div>
-                    
+                    <h3>
+                        Lifestyle:
+                        <input
+                            type="checkbox"
+                            id="option-a"
+                            value="Lifestyle"
+                        />
+                    </h3>
+                    <h3>
+                        Self help:
+                        <input type="checkbox" id="option-b" value="Selfhelp" />
+                    </h3>
+                    <h3>
+                        Novel:
+                        <input type="checkbox" id="option-c" value="Novel" />
+                    </h3>
+                    <h3>
+                        Sport:
+                        <input type="checkbox" id="option-d" value="Sport" />
+                    </h3>
+                    <h3>
+                        Sport:
+                        <input type="checkbox" id="option-e" value="Sport" />
+                    </h3>
+                    <h3>
+                        Sport:
+                        <input type="checkbox" id="option-f" value="Sport" />
+                    </h3>
+                    <h3>
+                        Sport:
+                        <input type="checkbox" id="option-g" value="Sport" />
+                    </h3>
+                </div>
             </form>
         </div>
     );
-};
+}
